e4d523eb67f66ccbd8dd498755eb92f91bb9c8bb
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-10-20 14:46:25 +0700

    setup interval

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const INTERVAL_PING_ROOM = 270000;
+const START_RECORD_TIME = 6; //6h
+const END_RECORD_TIME = 23; //6h
+
 var request = require("request");
 
 let gateway = '192.168.1.0-255';
@@ -5,29 +9,31 @@ if(process.argv[2])
 	gateway = process.argv[2];
 let cmd = `sudo nmap -sP ${gateway}`;
 
-console.time('nmap -sP');
-
+/**
+ * MAIN LOGIC, exec nmap command, then post data to server
+ * @return {[type]} [description]
+ */
 let pingRoom = function(){
-	let exec = require('child_process').exec;
-	let c1 = exec(cmd, parseResult);
+	let hour = new Date().getHours();
+	if(hour >= START_RECORD_TIME && hour <= END_RECORD_TIME){
+		let exec = require('child_process').exec;
+		let c1 = exec(cmd, parseResult);
+	}
 };
 
+let interval = setInterval(pingRoom, INTERVAL_PING_ROOM);
 
-//DEBUG
-// let repl = require('repl');
+process.on('exit', ()=>{
+	console.log('bye-bye');
+	clearInterval(interval);
+});
 
 function parseResult(err, stdout) {
 	if (err) throw err;
 
-	console.timeEnd('nmap -sP');
-
 	let result = require(`${__dirname}/parse-report`)(stdout.toString());
-
-	// repl.start('>').context.result = result;
-	// console.log(result);
 	updateDb(result.devices);
 }
-
 
 function updateDb(result) {
 	console.log(JSON.stringify(result));
